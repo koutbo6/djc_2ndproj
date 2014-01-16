5ad0c8d2656e0d38b476538826aad54c36f7029e
Mohammad AlMarzouq <m.almarzouq@kbsoft.com.kw>
2014-01-16 13:41:17 +0300

    product and rating admin

--- rating/admin.py
+++ rating/admin.py
@@ -3,15 +3,15 @@ from django.contrib import admin
 from .models import Product, Rating
 
 class RatingAdmin(admin.ModelAdmin):
-    pass
+    raw_id_fields = ('product', )
 
 class ProductAdmin(admin.ModelAdmin):
     # Fields to show in list view
     list_display = ('name', 'url', 'category', )
-    # # Fields we can filter on
-    # list_filter = ('category', )
-    # # Fields we can search in admin
-    # search_fields = ('name', )
+    # Fields we can filter on
+    list_filter = ('category', )
+    # Fields we can search in admin
+    search_fields = ('name', )
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Rating, RatingAdmin)
