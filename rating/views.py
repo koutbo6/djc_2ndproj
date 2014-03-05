ccc46c1ffeff0288cbed8a0c6db4918900944b36
Mohammad AlMarzouq <m.almarzouq@kbsoft.com.kw>
2014-03-05 11:10:49 +0300

    using factory methods for modelforms and modelform sets

--- rating/views.py
+++ rating/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import  Product, Rating
 from .forms import ProductForm, RatingForm
 from django.forms.formsets import formset_factory
+from django.forms.models import modelform_factory, inlineformset_factory, modelformset_factory
 
 # Create your views here.
 def product_list(request):
@@ -19,13 +20,16 @@ def product_details(request, pid):
         {"product": obj, },
     )
 
+# multiple product entry
 def product_list_entry(request):
     if not request.user.is_authenticated():
         # fetch an authenticated user (demo only)
         u = authenticate(username="foo", password="123")
         login(request, u)
 
-    ProductFormSet = formset_factory(ProductForm, extra=0)
+    # this changed, param is model rather than form
+    # everything else is the same
+    ProductFormSet = modelformset_factory(Product, extra=0)
     
     if request.method == "POST":
         formset = ProductFormSet(request.POST)
@@ -47,24 +51,74 @@ def product_list_entry(request):
     )
 
 def product_rate(request, pid):
-        # login the user if he/she is not
+    # login the user if he/she is not
     if not request.user.is_authenticated():
         # fetch an authenticated user
         u = authenticate(username="foo", password="123")
         login(request, u)
-
     product = get_object_or_404(Product, pk=pid)
+
+    # only difference is here
+    # no import for ModelForm needed
+    # factory method creates the class
+    # on the fly, everything else is the same
+    NewRatingForm = modelform_factory(Rating)
     if request.method == "POST":
-        form = RatingForm(request.POST)
+        form = NewRatingForm(request.POST)
         if form.is_valid():
             Rating.objects.create(**form.cleaned_data)
             return redirect(form.cleaned_data["product"])
     else:
-        form = RatingForm(initial={"reviewer":request.user, "product":product})
+        form = NewRatingForm(initial={"reviewer":request.user, "product":product})
     return render(
         request,
         "rating/product_rate.html",
         {"form": form, "product": product}
+    )
+
+
+def mass_edit_product_rate(request, pid):
+    # login the user if he/she is not
+    if not request.user.is_authenticated():
+        # fetch an authenticated user
+        u = authenticate(username="foo", password="123")
+        login(request, u)
+    product = get_object_or_404(Product, pk=pid)
+
+
+
+    # create the InlineFormSet class on the fly
+    MassRatingFormSet = inlineformset_factory(Product, 
+        Rating, extra=3, can_delete=False)
+    # everything else is almost the same
+    if request.method == "POST":
+        # slight difference is the instance param
+        # this is used to specify the parent instance
+        formset = MassRatingFormSet(request.POST, instance=product)
+        if formset.is_valid():
+            for item in formset:
+                obj = item.cleaned_data.get("id")
+                # update current object
+                if obj:
+                    for k,v in item.cleaned_data.items():
+                        if hasattr(obj, k) and k != "id":
+                            setattr(obj, k, v)
+                    obj.save()
+                else:
+                    Rating.objects.create(**item.cleaned_data)
+            
+            return redirect('product_list')
+    else:
+        # Remember to set instance here as well
+        # initial values will be populated correctly
+        formset = MassRatingFormSet(instance=product)
+        
+    return render(
+        request,
+        "rating/product_list_entry.html",
+        {
+            "formset":formset,
+        },
     )
 
 def create_product(request):
